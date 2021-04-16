d86fc308fa11791d971881e9e320eaa1bfed7681
tdenzl <63013310+tdenzl@users.noreply.github.com>
2021-04-16 20:43:10 +0200

    Update BuLiAn.py

--- BuLiAn.py
+++ BuLiAn.py
@@ -312,8 +312,8 @@ def plt_attribute_scatter(aspect1, aspect2):
 ####################
 
 row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.beta_columns((.1, 2.3, .1, 1.3, .1))
-row0_1.title('BuLiAn - Bundesliga Analyzer (WIP)')
-row0_2.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
+row0_1.title('BuLiAn - Bundesliga Analyzer')
+row0_2.subheader('WIP Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
 row3_spacer1, row3_1, row3_spacer2 = st.beta_columns((.1, 3.2, .1))
 with row3_1:
     st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' and how your club 'just won more tackles last season' ? However, you did not want to start an argument because you did not have any stats at hand? Well this simple application containing Bundesliga data from seasons 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
