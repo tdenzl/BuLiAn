3638ffaedea24437706ade7b984949967a239cdb
tdenzl <63013310+tdenzl@users.noreply.github.com>
2021-05-04 17:21:01 +0200

    Add files via upload

--- BuLiAn.py
+++ BuLiAn.py
@@ -407,7 +407,7 @@ df_data_filtered_matchday = filter_matchday(df_data_filtered_season)
 
 ### TEAM SELECTION ###
 unique_teams = get_unique_teams(df_data_filtered_matchday)
-all_teams_selected = st.sidebar.selectbox('Do you want to focus on more more specific teams? If the answer is yes, please check the box below and then select the specfic team(s) in the new field.', ['Include all available teams','Select teams manually (choose below)'])
+all_teams_selected = st.sidebar.selectbox('Do you want to only include specific teams? If the answer is yes, please check the box below and then select the team(s) in the new field.', ['Include all available teams','Select teams manually (choose below)'])
 if all_teams_selected == 'Select teams manually (choose below)':
     selected_teams = st.sidebar.multiselect("Select and deselect the teams you would like to include in the analysis? You can clear the current selection by clicking the corresponding x-button on the right", unique_teams, default = unique_teams)
 df_data_filtered = filter_teams(df_data_filtered_matchday)        
