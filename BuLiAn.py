b650a2ff59e72e045f925ae570c46d5c57c3afb5
tdenzl <63013310+tdenzl@users.noreply.github.com>
2021-05-07 14:45:38 +0200

    Add files via upload

--- BuLiAn.py
+++ BuLiAn.py
@@ -386,7 +386,7 @@ with row0_2:
 row3_spacer1, row3_1, row3_spacer2 = st.beta_columns((.1, 3.2, .1))
 with row3_1:
     st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
-    st.markdown("You can find the source code in the [GitHub BuLiAn Repository](https://github.com/tdenzl/BuLiAn)")
+    st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
     st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
     
 #################
