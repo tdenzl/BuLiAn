bcfff777ce42dffdfd1f8396781c8f742c1ca81e
tdenzl <63013310+tdenzl@users.noreply.github.com>
2021-09-30 08:35:55 +0200

    Add files via upload

--- BuLiAn.py
+++ BuLiAn.py
@@ -377,13 +377,13 @@ def build_matchfacts_return_string(return_game_id_value_team,min_max,attribute,w
 ### INTRODUCTION ###
 ####################
 
-row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.beta_columns((.1, 2.3, .1, 1.3, .1))
+row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
 with row0_1:
     st.title('BuLiAn - Bundesliga Analyzer')
 with row0_2:
     st.text("")
     st.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
-row3_spacer1, row3_1, row3_spacer2 = st.beta_columns((.1, 3.2, .1))
+row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 with row3_1:
     st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
     st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
@@ -410,16 +410,16 @@ df_data_filtered_matchday = filter_matchday(df_data_filtered_season)
 
 ### TEAM SELECTION ###
 unique_teams = get_unique_teams(df_data_filtered_matchday)
-all_teams_selected = st.sidebar.selectbox('Do you want to only include specific teams?', ['Include all available teams','Select teams manually (choose below)'])
+all_teams_selected = st.sidebar.selectbox('Do you want to only include specific teams? If the answer is yes, please check the box below and then select the team(s) in the new field.', ['Include all available teams','Select teams manually (choose below)'])
 if all_teams_selected == 'Select teams manually (choose below)':
     selected_teams = st.sidebar.multiselect("Select and deselect the teams you would like to include in the analysis. You can clear the current selection by clicking the corresponding x-button on the right", unique_teams, default = unique_teams)
 df_data_filtered = filter_teams(df_data_filtered_matchday)        
 ### SEE DATA ###
-row6_spacer1, row6_1, row6_spacer2 = st.beta_columns((.2, 7.1, .2))
+row6_spacer1, row6_1, row6_spacer2 = st.columns((.2, 7.1, .2))
 with row6_1:
     st.subheader("Currently selected data:")
 
-row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3, row2_3, row2_spacer4, row2_4, row2_spacer5   = st.beta_columns((.2, 1.6, .2, 1.6, .2, 1.6, .2, 1.6, .2))
+row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3, row2_3, row2_spacer4, row2_4, row2_spacer5   = st.columns((.2, 1.6, .2, 1.6, .2, 1.6, .2, 1.6, .2))
 with row2_1:
     unique_games_in_df = df_data_filtered.game_id.nunique()
     str_games = "🏟️ " + str(unique_games_in_df) + " Matches"
@@ -440,7 +440,7 @@ with row2_4:
     str_shots = "👟⚽ " + str(total_shots_in_df) + " Shots"
     st.markdown(str_shots)
 
-row3_spacer1, row3_1, row3_spacer2 = st.beta_columns((.2, 7.1, .2))
+row3_spacer1, row3_1, row3_spacer2 = st.columns((.2, 7.1, .2))
 with row3_1:
     st.markdown("")
     see_data = st.beta_expander('You can click here to see the raw data first 👉')
@@ -458,23 +458,23 @@ st.text('')
 ################
 
 ### DATA EXPLORER ###
-row12_spacer1, row12_1, row12_spacer2 = st.beta_columns((.2, 7.1, .2))
+row12_spacer1, row12_1, row12_spacer2 = st.columns((.2, 7.1, .2))
 with row12_1:
     st.subheader('Match Finder')
     st.markdown('Show the (or a) match with the...')  
 if all_teams_selected == 'Include all available teams':
-    row13_spacer1, row13_1, row13_spacer2, row13_2, row13_spacer3, row13_3, row13_spacer4   = st.beta_columns((.2, 2.3, .2, 2.3, .2, 2.3, .2))
+    row13_spacer1, row13_1, row13_spacer2, row13_2, row13_spacer3, row13_3, row13_spacer4   = st.columns((.2, 2.3, .2, 2.3, .2, 2.3, .2))
     with row13_1:
         show_me_hi_lo = st.selectbox ("", ["Maximum","Minimum"],key = 'hi_lo') 
     with row13_2:
         show_me_aspect = st.selectbox ("", list(label_fact_dict.keys()),key = 'what')
     with row13_3:
         show_me_what = st.selectbox ("", ["by a team", "by both teams", "difference between teams"],key = 'one_both_diff')
-    row14_spacer1, row14_1, row14_spacer2 = st.beta_columns((.2, 7.1, .2))
+    row14_spacer1, row14_1, row14_spacer2 = st.columns((.2, 7.1, .2))
     with row14_1:
         return_game_id_value_team = find_match_game_id(show_me_hi_lo,show_me_aspect,show_me_what)
         df_match_result = build_matchfacts_return_string(return_game_id_value_team,show_me_hi_lo,show_me_aspect,show_me_what)     
-    row15_spacer1, row15_1, row15_2, row15_3, row15_4, row15_spacer2  = st.beta_columns((0.5, 1.5, 1.5, 1, 2, 0.5))
+    row15_spacer1, row15_1, row15_2, row15_3, row15_4, row15_spacer2  = st.columns((0.5, 1.5, 1.5, 1, 2, 0.5))
     with row15_1:
         st.subheader(" ‎")
     with row15_2:
@@ -486,12 +486,12 @@ if all_teams_selected == 'Include all available teams':
     with row15_4:
         st.subheader(str(df_match_result.iloc[1]['team']))
 else:
-    row17_spacer1, row17_1, row17_spacer2 = st.beta_columns((.2, 7.1, .2))
+    row17_spacer1, row17_1, row17_spacer2 = st.columns((.2, 7.1, .2))
     with row17_1:
         st.warning('Unfortunately this analysis is only available if all teams are included')
 
 if all_teams_selected == 'Include all available teams':
-    row16_spacer1, row16_1, row16_2, row16_3, row16_4, row16_spacer2  = st.beta_columns((0.5, 1.5, 1.5, 1, 2, 0.5))
+    row16_spacer1, row16_1, row16_2, row16_3, row16_4, row16_spacer2  = st.columns((0.5, 1.5, 1.5, 1, 2, 0.5))
     with row16_1:
         st.markdown("👟 Shots on Goal")
         st.markdown("🏃‍♂️ Distance (in km)")
@@ -520,10 +520,10 @@ if all_teams_selected == 'Include all available teams':
 
 
 ### TEAM ###
-row4_spacer1, row4_1, row4_spacer2 = st.beta_columns((.2, 7.1, .2))
+row4_spacer1, row4_1, row4_spacer2 = st.columns((.2, 7.1, .2))
 with row4_1:
     st.subheader('Analysis per Team')
-row5_spacer1, row5_1, row5_spacer2, row5_2, row5_spacer3  = st.beta_columns((.2, 2.3, .4, 4.4, .2))
+row5_spacer1, row5_1, row5_spacer2, row5_2, row5_spacer3  = st.columns((.2, 2.3, .4, 4.4, .2))
 with row5_1:
     st.markdown('Investigate a variety of stats for each team. Which team scores the most goals per game? How does your team compare in terms of distance ran per game?')    
     plot_x_per_team_selected = st.selectbox ("Which attribute do you want to analyze?", list(label_attr_dict_teams.keys()), key = 'attribute_team')
@@ -536,10 +536,10 @@ with row5_2:
         st.warning('Please select at least one team')
 
 ### SEASON ###
-row6_spacer1, row6_1, row6_spacer2 = st.beta_columns((.2, 7.1, .2))
+row6_spacer1, row6_1, row6_spacer2 = st.columns((.2, 7.1, .2))
 with row6_1:
     st.subheader('Analysis per Season')
-row7_spacer1, row7_1, row7_spacer2, row7_2, row7_spacer3  = st.beta_columns((.2, 2.3, .4, 4.4, .2))
+row7_spacer1, row7_1, row7_spacer2, row7_2, row7_spacer3  = st.columns((.2, 2.3, .4, 4.4, .2))
 with row7_1:
     st.markdown('Investigate developments and trends. Which season had teams score the most goals? Has the amount of passes per games changed?')    
     plot_x_per_season_selected = st.selectbox ("Which attribute do you want to analyze?", list(label_attr_dict.keys()), key = 'attribute_season')
@@ -551,10 +551,10 @@ with row7_2:
         st.warning('Please select at least one team')
 
 ### MATCHDAY ###
-row8_spacer1, row8_1, row8_spacer2 = st.beta_columns((.2, 7.1, .2))
+row8_spacer1, row8_1, row8_spacer2 = st.columns((.2, 7.1, .2))
 with row8_1:
     st.subheader('Analysis per Matchday')
-row9_spacer1, row9_1, row9_spacer2, row9_2, row9_spacer3  = st.beta_columns((.2, 2.3, .4, 4.4, .2))
+row9_spacer1, row9_1, row9_spacer2, row9_2, row9_spacer3  = st.columns((.2, 2.3, .4, 4.4, .2))
 with row9_1:
     st.markdown('Investigate stats over the course of a season. At what point in the season do teams score the most goals? Do teams run less towards the end of the season?')    
     plot_x_per_matchday_selected = st.selectbox ("Which aspect do you want to analyze?", list(label_attr_dict.keys()), key = 'attribute_matchday')
@@ -570,10 +570,10 @@ with row9_2:
 ### CORRELATION ###
 corr_plot_types = ["Regression Plot (Recommended)","Standard Scatter Plot"] #removed "Violin Plot (High Computation)"
 
-row10_spacer1, row10_1, row10_spacer2 = st.beta_columns((.2, 7.1, .2))
+row10_spacer1, row10_1, row10_spacer2 = st.columns((.2, 7.1, .2))
 with row10_1:
     st.subheader('Correlation of Game Stats')
-row11_spacer1, row11_1, row11_spacer2, row11_2, row11_spacer3  = st.beta_columns((.2, 2.3, .4, 4.4, .2))
+row11_spacer1, row11_1, row11_spacer2, row11_2, row11_spacer3  = st.columns((.2, 2.3, .4, 4.4, .2))
 with row11_1:
     st.markdown('Investigate the correlation of attributes, but keep in mind correlation does not imply causation. Do teams that run more than their opponents also score more goals? Do teams that have more shots than their opponents have more corners?')    
     corr_type = st.selectbox ("What type of correlation plot do you want to see?", corr_plot_types)
